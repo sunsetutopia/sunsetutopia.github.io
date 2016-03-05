08fe131c9ec5fa793f5ac2e04d1ee8d0e829fb32
James Livingston <doc@sunsetutopia.com>
2016-03-05 11:08:31 +1100

    tidy up config

--- serve.sh
+++ serve.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-$HOME/.gem/ruby/gems/bundler-1.10.6/bin/bundle exec jekyll serve --watch
+#$HOME/.gem/ruby/gems/bundler-1.10.6/bin/bundle exec jekyll serve --watch
+bundle exec jekyll serve --watch
